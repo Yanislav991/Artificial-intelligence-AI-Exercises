2697dafdbc5579151d9f70bacc96dc624c926e2b
unknown <frexcho998@gmail.com>
2023-04-01 09:44:11 +0300

    fix huffman tree

--- Math Concepts For Developers/Algorithms/huffman_tree.py
+++ Math Concepts For Developers/Algorithms/huffman_tree.py
@@ -1,43 +1,42 @@
 class Node():
-    def __init__(self, left=None, right=None):
+    def __init__(self, freq, char=None,  left=None, right=None):
         self.left = left
         self.right = right
-
-    def children(self):
-        return (self.left, self.right)
-
-    def nodes(self):
-        return (self.left, self.right)
+        self.char = char
+        self.freq = freq
 
     def __str__(self):
         return '%s_%s' % (self.left, self.right)
 
 
+def create_node(t):
+    return Node(t[1], t[0])
+
+
+def create_huffman_tree(nodes):
+    while len(nodes) >= 1:
+        left_node = nodes.pop(0)
+        if (len(nodes) == 0):
+            return left_node
+        right_node = nodes.pop(0)
+        nodes.append(Node(left_node.freq + right_node.freq,
+                     left=left_node, right=right_node))
+        nodes = sorted(nodes, key=lambda item: item.freq)
+
+
 text = input()
-freq ={}
+freq = {}
 
 for c in text:
     if c in freq:
-        freq[c] +=1
+        freq[c] += 1
     else:
         freq[c] = 1
 
 freq = sorted(freq.items(), key=lambda item: item[1])
-nodes = []
 
-while len(freq) != 0:
-    newNode = Node()
-    if(freq[0] is not None):
-        first_min = freq[0]
-        newNode.left = first_min
-        if(2<len(freq)):
-            second_min = freq[1]
-            newNode.right = second_min
-            freq.remove(freq[1])    
-        freq.remove(freq[0])
-    value = newNode.left[1]
-    if newNode.right is not None:
-        value += newNode.right[1]
-    nodes.append((newNode, value))
+nodes = list(map(create_node, freq))
 
-print(nodes)
+huffman_tree = create_huffman_tree(nodes)
+
+print(huffman_tree)
