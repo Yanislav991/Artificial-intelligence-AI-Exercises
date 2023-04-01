4ded60cd4b1a8f472a926d32d844e8ec00fcbc12
unknown <frexcho998@gmail.com>
2023-04-01 10:42:42 +0300

    added functions to calculate the size of the compressed text along with the tree data

--- Math Concepts For Developers/Algorithms/huffman_tree.py
+++ Math Concepts For Developers/Algorithms/huffman_tree.py
@@ -5,13 +5,14 @@ class Node():
         self.char = char
         self.freq = freq
 
+    def children(self):
+        return (self.left, self.right)
+
     def __str__(self):
         return '%s_%s' % (self.left, self.right)
 
-
 def create_node(t):
     return Node(t[1], t[0])
-
 
 def create_huffman_tree(nodes):
     while len(nodes) >= 1:
@@ -23,8 +24,27 @@ def create_huffman_tree(nodes):
                      left=left_node, right=right_node))
         nodes = sorted(nodes, key=lambda item: item.freq)
 
+def huffman_code_tree(node, left=True, binString=''):
+    if node.char is not None:
+        return {node.char: (node.freq, binString)}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
 
-text = input()
+def calc_tree_size(dict):
+    values = dict.values()
+    result = 0
+    for v in values:
+        result += v[0]*len(v[1])
+        result += v[0]
+    result += len(values)*8
+    return result
+
+
+
+text = 'BCAADDDCCACACAC'
 freq = {}
 
 for c in text:
@@ -34,9 +54,18 @@ for c in text:
         freq[c] = 1
 
 freq = sorted(freq.items(), key=lambda item: item[1])
-
 nodes = list(map(create_node, freq))
-
 huffman_tree = create_huffman_tree(nodes)
 
-print(huffman_tree)
+encoded_values = huffman_code_tree(huffman_tree)
+totalSize = 0
+initial_size = len(text) * 8
+tree_size = calc_tree_size(encoded_values)
+
+print(' Char | Huffman code ')
+print('----------------------')
+for (char, frequency) in freq:
+    print(' %-4r |%12s' % (char, encoded_values[char]))
+
+print(f"Intial Size -> {initial_size}")
+print(f"Tree Size -> {tree_size}")
